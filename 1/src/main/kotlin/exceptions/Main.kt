83f473049191e4431802f4c167a2db6aa18e93e2
Yulia Mihaylova <yulu.2000@hotmail.com>
2021-04-20 22:02:07 +0300

    exceptions and collections

--- 1/src/main/kotlin/exceptions/Main.kt
+++ 1/src/main/kotlin/exceptions/Main.kt
@@ -1,10 +1,29 @@
 package exceptions
 
+import java.io.BufferedReader
+import java.io.ObjectInput
+import java.io.StringReader
+import java.lang.NumberFormatException
+
 fun main() {
     println(isPercentage(11))
-    println(isPercentage(101))
+    // println(isPercentage(101))
+    println(readNumber(BufferedReader(StringReader("123"))))
 }
 
 fun isPercentage(number: Int) : Int {
     return if (number in 1..100) number else throw IllegalArgumentException()
+}
+
+fun readNumber(reader: BufferedReader) : Int? {
+    return try {
+        Integer.parseInt(reader.readLine())
+    }
+    catch (e: NumberFormatException) {
+        println("Not a number")
+        null
+    }
+    finally {
+        reader.close()
+    }
 }
